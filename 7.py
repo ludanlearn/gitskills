61c45adf42c0d0ab7fd46a550681d8060de06cab
ludan <luludandan8@163.com>
2017-05-15 17:56:45 +0800

    7test

--- 7.py
+++ 7.py
@@ -1,2 +1,11 @@
 from PIL import Image
-im = Image.open('d:oxygen.png')
+im = Image.open('d:/oxygen.png')
+w,h = im.size
+print(w,h)
+L = []
+for i in [(x,y) for x in range(1,w) for y in range(1,h)]:
+    s = im.getpixel(i)
+    if s[0] == s[1] ==s[2]:
+        L.append(chr(i[0]))
+k = ''.join(L)
+print(K.decode('utf-8'))
