f76b0ed52d2cffed3048339b27a9dabedff16264
ludan <luludandan8@163.com>
2017-05-17 00:48:08 +0800

    完成第七题

--- 7.py
+++ 7.py
@@ -2,10 +2,15 @@ from PIL import Image
 im = Image.open('d:/oxygen.png')
 w,h = im.size
 print(w,h)
-L = []
-for i in [(x,y) for x in range(1,w) for y in range(1,h)]:
+L,g= [],[]
+n = 0
+for i in [(x,50) for x in range(1,w)]:
     s = im.getpixel(i)
-    if s[0] == s[1] ==s[2]:
-        L.append(chr(i[0]))
-k = ''.join(L)
-print(K.decode('utf-8'))
+    if s[0] == s[1] == s[2]:
+       L.append(chr(s[0]))
+while n < len(L):
+    g.append(L[n])
+    n = n + 7
+print(''.join(g).encode('utf-8'))
+    
+    
